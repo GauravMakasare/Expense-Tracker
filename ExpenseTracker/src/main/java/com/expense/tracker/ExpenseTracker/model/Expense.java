e53fb1c4f7104f0292ab514816d1dd601c3e9812
GauravMakasare <gauravmakasare115@gmail.com>
2021-06-23 22:39:53 +0530

    Adding Expense class
    this class will hold the data of all the expenses of a user
    
    Instant is used to add time stamps for the data
    
    [annotatios
    many Expenses under one category
    and also many expenses done by the user]

--- ExpenseTracker/src/main/java/com/expense/tracker/ExpenseTracker/model/Expense.java
+++ ExpenseTracker/src/main/java/com/expense/tracker/ExpenseTracker/model/Expense.java
@@ -1,5 +1,35 @@
 package com.expense.tracker.ExpenseTracker.model;
 
-public class Expense {
+import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(name = "expense")
+
+public class Expense {
+	
+	@Id
+	private Long id;
+	
+	private Instant expenseDate; //An instantaneous point on the time-line to add timestamps 
+	
+	private String description;
+	
+	@ManyToOne
+	private Category category;
+	
+	@ManyToOne
+	private User user;
+	
 }
