3f1e80ff62fb85ffca0d104b59bd4103b61c1e5e
GauravMakasare <gauravmakasare115@gmail.com>
2021-07-02 22:52:10 +0530

    adding more Routes and completing the API

--- ExpenseApp-master/ExpenseApp-master/src/main/java/com/expensetracker/expense/controller/CategoryController.java
+++ ExpenseApp-master/ExpenseApp-master/src/main/java/com/expensetracker/expense/controller/CategoryController.java
@@ -9,9 +9,11 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,16 +38,28 @@ public class CategoryController {
 	}
 
 	@GetMapping("/category/{id}")
-		ResponseEntity<?> getCategory(@PathVariable Long id){
+	ResponseEntity<?> getCategory(@PathVariable Long id) {
 		Optional<Category> category = categoryRepository.findById(id);
-		 return category.map(response -> ResponseEntity.ok().body(response))
-				 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+		return category.map(response -> ResponseEntity.ok().body(response))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 
 	@PostMapping("/category")
-	ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) throws URISyntaxException{
-		 Category result = categoryRepository.save(category);
-		 return ResponseEntity.created(new URI("/api/category" + result.getId())).body(result);
+	ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) throws URISyntaxException {
+		Category result = categoryRepository.save(category);
+		return ResponseEntity.created(new URI("/api/category" + result.getId())).body(result);
+
+	}
+
+	@PutMapping("/category/{id}") // if we want to overwrite any data we will use this type of mapping
+	ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category) {
+		Category result = categoryRepository.save(category);
+		return ResponseEntity.ok().body(result);
 	}
 	
+	@DeleteMapping("/category/{id}")
+	ResponseEntity<?> deleteCategory (@PathVariable Long id){
+		categoryRepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
 }
