2804652cf1b1f022f540c0e12118268cd6e8235b
GauravMakasare <gauravmakasare115@gmail.com>
2021-07-05 00:56:23 +0530

    UI improvization

---
+++ ExpenseApp-master/ExpenseApp-master/expenseapp/src/AppNav.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {Nav,Navbar,NavItem,NavbarBrand, NavLink} from 'reactstrap';
+
+
+class AppNav extends Component {
+    state = {  }
+    render() {
+        return (
+          <div>
+            <Navbar color="light" light expand="md">
+              <NavbarBrand href="/">Expense Tracker Application</NavbarBrand>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/home">Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/categories">Categories</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/expenses">Expenses</NavLink>
+                  </NavItem>
+                </Nav>
+            </Navbar>
+          </div>
+        );
+      }
+}
+ 
+export default AppNav;
--- ExpenseApp-master/ExpenseApp-master/expenseapp/src/Category.js
+++ ExpenseApp-master/ExpenseApp-master/expenseapp/src/Category.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react';
+import AppNav from './AppNav';
 
 class Category extends Component {
-    state = { 
+
+    state = {  
         isLoading : true,
         Categories : []
-     }
+    }
 
-     //there are two ways to send a request in Js 
-     // Sync - you send the request and then wait for the response
-     // Async - you send the request and we dont have to wait until the response is ready 
-
-     async componentDidMount(){
-
-        const response = await fetch('/api/categories')
+    //there are two ways to send a request
+    //sync - send and wait for the response 
+    //async - send and we do not have to keep waiting for the response
+ 
+    async componentDidMount(){
+        const response=await fetch('/api/categories');
         const body = await response.json();
-        this.setState({Categories : body , isLoading :false});
-
-     }
-
+        this.setState({Categories : body , isLoading: false});
+    }
 
     render() { 
         const {Categories , isLoading} = this.state;
-        if(isLoading)
-            return (<div>Loading. . .</div>);
-        //iterating through each category and priting the names and the id of each id 
-        return (
-            <div>
-                <h2>Categories</h2>
-                {
-                Categories.map( category =>
+        if(isLoading) 
+            return (<div>Loading...</div>);
+        
+        return ( 
+            
+                <div>
+                    <AppNav/>
+                    <h2>Categories</h2>
+                    {
+                        Categories.map( category => 
                             <div id={category.id}>
-                                {category.name}     
-                            </div>       
+                                {category.name}
+                            </div>
+                        )
 
-                            )
-                }
-            </div>
-        );
+                    }
+
+                </div>
+         );
     }
 }
  
